6cc41a70c2ef2cf10083f96edd4a72c16bd598a2
Have Samuel <samiehave9@gmail.com>
2023-05-18 02:46:23 +0300

    Create the getMovies Function

--- scripts/app.js
+++ scripts/app.js
@@ -1,25 +1,16 @@
 /* eslint-disable no-undef */
+// Catch the submission of the form and send the data using AJAX
 
-// When the form is submitted,
-// capture the values for each of the inputs and append them to the DOM
-// along with a button to remove each title and rating from the DOM.
-// When the button to remove is clicked, remove each title and rating from the DOM.
+// getMovies Functiono
+function getMovies(searchText) {
+  console.log(searchText);
+}
 
-$('form').on('submit', (ev) => {
-  ev.preventDefault();
-  const $title = $('#title').val();
-  const $rating = $('#rating').val();
-  const $newMovie = $(`<li>${$title} is rated ${$rating}</li>`);
-  const $removeButton = $('<button>Remove</button>');
-  $newMovie.append($removeButton);
-  $('ul').append($newMovie);
-
-  $removeButton.on('click', () => {
-    $newMovie.remove();
+$(document).ready(() => {
+  // Create an Event Listener for the form submission
+  $('#searchForm').on('submit', (eve) => {
+    const searchText = $('#searchText').val();
+    getMovies(searchText);
+    eve.preventDefault();
   });
-
-  $('form').trigger('reset');
 });
-
-// When the user clicks on the button to remove the movie,
-// remove the movie from the DOM and the array of movies.
