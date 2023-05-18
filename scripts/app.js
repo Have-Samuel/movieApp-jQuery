390d455bffc4c639b832b14eecb1282b77f1f11a
Have Samuel <samiehave9@gmail.com>
2023-05-18 04:02:26 +0300

    Creating the details and back to search Buttons

--- scripts/app.js
+++ scripts/app.js
@@ -54,7 +54,7 @@ function getMovie() {
       </div>
       <div class="col-md-8">
       <h2>${movie.Title}</h2>
-      <ul class="list-group">
+      <ul class="list-group">  
         <li class="list-group-item"><strong>Genre:</strong>${movie.Genre}</li>
         <li class="list-group-item"><strong>Released:</strong>${movie.Released}</li>
         <li class="list-group-item"><strong>Rated:</strong>${movie.Rated}</li>
@@ -63,6 +63,15 @@ function getMovie() {
         <li class="list-group-item"><strong>Writer:</strong>${movie.Writer}</li>
         <li class="list-group-item"><strong>Actors:</strong>${movie.Actors}</li>
       </ul>
+      </div>
+    </div>
+    <div class="row">
+      <div class="well">
+        <h3>Plot</h3>
+        ${movie.Plot}
+        <hr>
+        <a href="http://imdb.com/title/${movie.imdbID}" target="_blank" class="btn btn-primary">View IMDB</a>
+        <a href="index.html" class="btn btn-default">Go Back To Search</a>
       </div>
     </div>
     `;
