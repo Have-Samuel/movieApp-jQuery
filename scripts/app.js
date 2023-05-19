3cdadf8ed9c8018b06d6faaaea5aa7d355aae35c
Have Samuel <samiehave9@gmail.com>
2023-05-20 00:17:53 +0300

    Finding another way to get data and Add Axios

--- scripts/app.js
+++ scripts/app.js
@@ -12,25 +12,32 @@ $(document).ready(() => {
     const movies = $('#searchText').val();
 
     // Declare our result variable
-    let result = '';
+    // let result = '';
 
     // const url = `http://www.omdbapi.com/?apikey=${apiKey}`;
-    const url = `http://www.omdbapi.com/?i=tt3896198&apikey=${apiKey}`;
+    // const url = `http://www.omdbapi.com/?i=tt3896198&apikey=${apiKey}`;
 
     // http:// www.omdbapi.com/?i=tt3896198&apikey=${apiKey}
 
     // Make an AJAX request to the OMDB API
-    $.ajax({
-      method: 'GET',
-      url: `${url}&t=${movies}`,
-      success: (data) => {
-        console.log(data);
+    // $.ajax({
+    //   method: 'GET',
+    //   url: `${url}&t=${movies}`,
+    //   success: (data) => {
+    //     console.log(data);
 
-        result = `
-          <img style="float:left" class="img-thumbnail" width="200" height="200" src=${data.Poster}/>
-        `;
-        $('#movies').html(result);
-      },
-    });
+    //     result = `
+    //       <img style="float:left" class="img-thumbnail" width="200" height="200" src=${data.Poster}/>
+    //     `;
+    //     $('#movies').html(result);
+    //   },
+    // });
+    axios.get(`http://www.omdbapi.com/?i=tt3896198&apikey=${apiKey}=${movies}`)
+      .then((response) => {
+        console.log(response);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
   });
 });
