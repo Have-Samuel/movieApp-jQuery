55fc15e0b120aed714b8e332a382e85db61ec3f5
Have Samuel <samiehave9@gmail.com>
2023-05-25 01:00:52 +0300

    Displaying single movie

--- scripts/app.js
+++ scripts/app.js
@@ -41,32 +41,38 @@ $(document).ready(() => {
 
 // passing data from one page to another
 function movieSelected(id) {
-  sessionStorage.setItem(movieId, id);
+  sessionStorage.setItem('movieId', id);
   window.location = 'movie.html';
   return false;
 }
 
 function getMovie() {
-  let moveId = sessionStorage.setItem('movieId');
+  const movieId = sessionStorage.setItem('movieId');
 
   $.ajax({
     method: 'GET',
     url: `${url}&i=${movieId}`,
     success: (data) => {
-      // console.log(data);
-      const movies = data.Search;
-      $.each(movies, (index, movie) => {
-        result += `
-          <div class="col-md-3">
-            <div class="well text-center">
-              <img src="${movie.Poster}">
-              <h5>${movie.Title}</h5>
-              <a onclick="movieSelected('${movie.imdbID}')" class="btn btn-danger" href="#">Movie Details</a>
-            </div>
+      (
+        console.log(data)
+      );
+
+      const movie = data;
+
+      const result = `
+        <div class="row">
+          <div class="col-md-4">
+            <img src="${movie.Poster}" class="thumbnail">
           </div>
-        `;
-      });
-      $('#movies').html(result);
+            <div class="col-md-8">
+
+            </div>
+        </div>
+      `;
+
+      $('#movie').html(result);
     },
   });
 }
+
+// getMovie();
