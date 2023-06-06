599184e7a5717184d6e85534bff06aba72594931
Have Samuel <samiehave9@gmail.com>
2023-06-06 03:09:19 +0300

    Link Importation

--- scripts/app.js
+++
@@ -1,8 +0,0 @@
-/* eslint-disable no-undef */
-$(document).ready(() => {
-  $('body').on('submit', '#searchForm', (eve) => {
-    const searchText = $('#searchText').val();
-    alert(searchText);
-    eve.preventDefault();
-  });
-});
